cbf073b1eba6d0745d323053143defeb328fcf08
yavuzozay <57958423+yavuzozay@users.noreply.github.com>
2022-02-22 17:45:29 +0300

    create enemy

---
+++ BulletRush-Clone/Assets/Scripts/Enemy/BigEnemy.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigEnemy : Enemy
+{
+    protected override void Movement()
+    {
+        //Big Enemy Movement
+    }
+}
---
+++ BulletRush-Clone/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+
+    [SerializeField] protected float _healValue;
+    [SerializeField] protected float _speed;
+    [SerializeField]private GameObject particleEffect;
+
+    //Diðer classlardan eriþilebilir fakat deðiþtirilemez.
+    public float healValue
+    {
+        get { return _healValue; }
+    }
+
+    private void Update()
+    {
+        if (healValue <= 0)
+        {
+            Die();
+        }
+    }
+    private void Die()
+    {
+            Destroy(this.gameObject, .35f);
+    }
+    //Patlama efekti
+    private void OnDestroy()
+    {
+        Instantiate(particleEffect, transform.position, particleEffect.transform.rotation);
+    }
+
+    protected abstract void Movement();
+
+}
